a96112a484c0e9beffd529f0b3a1e298bafcdf45
Happiness Michael <111136082+Happinesspetra@users.noreply.github.com>
2025-02-18 20:54:07 +0100

    Update ErrorMessage.tsx

--- src/app/components/ErrorMessage.tsx
+++ src/app/components/ErrorMessage.tsx
@@ -1,16 +1,23 @@
+import Link from 'next/link';
+
 interface ErrorMessageProps {
-    message: string;
-  }
-  
-  export default function ErrorMessage({ message }: ErrorMessageProps) {
-    return (
-      <div className="rounded-md bg-black-50 dark:bg-black-900/30 p-4 my-4">
-        <div className="flex">
-          <div className="text-black-700 dark:text-black-200">
-            <h3 className="text-sm font-medium">Error!!!</h3>
-            <div className="mt-2 text-sm">{message}</div>
-          </div>
+  message: string;
+}
+
+export default function ErrorMessage({ message }: ErrorMessageProps) {
+  return (
+    <div className="rounded-md bg-gray-50 dark:bg-gray-900/30 p-4 my-4">
+      <div className="flex flex-col">
+        <div className="text-gray-700 dark:text-gray-200">
+          <h3 className="text-sm font-medium">Error!!!</h3>
+          <div className="mt-2 text-sm">{message}</div>
         </div>
+        <Link href="/">
+          <a className="mt-4 text-blue-600 dark:text-blue-400 hover:underline">
+            Go back to Home
+          </a>
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
+}
