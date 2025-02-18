3c4d61872d12fe3d779888245947f20d83c25f3c
Happiness Michael <111136082+Happinesspetra@users.noreply.github.com>
2025-02-18 20:50:29 +0100

    Update ErrorMessage.tsx

--- src/app/components/ErrorMessage.tsx
+++ src/app/components/ErrorMessage.tsx
@@ -7,7 +7,7 @@ interface ErrorMessageProps {
       <div className="rounded-md bg-black-50 dark:bg-black-900/30 p-4 my-4">
         <div className="flex">
           <div className="text-black-700 dark:text-black-200">
-            <h3 className="text-sm font-medium">Error</h3>
+            <h3 className="text-sm font-medium">Error!!!</h3>
             <div className="mt-2 text-sm">{message}</div>
           </div>
         </div>
